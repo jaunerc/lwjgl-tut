c95e5a4dc2164b56f70af1b0a0fa0adaacb8f95e
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2019-02-14 15:06:14 +0100

    commented

--- 02-Basic-Engine/src/main/java/ch/travbit/lwjgl/shapes/Circle.java
+++ 02-Basic-Engine/src/main/java/ch/travbit/lwjgl/shapes/Circle.java
@@ -4,6 +4,9 @@ import ch.travbit.lwjgl.engine.Entity;
 import ch.travbit.lwjgl.engine.opengl.Mesh;
 import ch.travbit.lwjgl.engine.ui.RgbaColor;
 
+/**
+ * This class represents a circle entity.
+ */
 public class Circle extends Entity {
 
     public Circle(Mesh mesh, int numPoints) {
@@ -13,11 +16,17 @@ public class Circle extends Entity {
     public Circle(Mesh mesh, int numPoints, RgbaColor fillColor) {
         super(mesh);
         float[] vertices = calculateVertices(numPoints);
-        int[] indices = calculateIndices(numPoints);
-        float[] colors = defineBasicColors(numPoints, fillColor);
+        int[] indices = defineTriangles(numPoints);
+        float[] colors = defineFillColors(numPoints, fillColor);
         mesh.storeBuffers(vertices, colors, indices);
     }
 
+    /**
+     * Calculates the vertices for this circle. This method generates a list of vertices. Each vertex has two
+     * coordinates. These coordinates are consecutive in the list. The first vertex is always the center of the circle.
+     * @param numPoints the number of points of the circle
+     * @return a list of vertices
+     */
     private float[] calculateVertices(int numPoints) {
         float[] vertices = new float[numPoints * 2 + 2];
         float angle = 2 * (float) Math.PI / numPoints;
@@ -38,7 +47,12 @@ public class Circle extends Entity {
         return vertices;
     }
 
-    private int[] calculateIndices(int numPoints) {
+    /**
+     * Defines the indices of the triangles to fill the circle.
+     * @param numPoints the number of points of the circle
+     * @return a list of triangle indices.
+     */
+    private int[] defineTriangles(int numPoints) {
         int[] triangles = new int[numPoints * 3 + 3];
 
         for (int i = 0, j = 0; i < numPoints + 1; i++, j+=3) {
@@ -50,7 +64,13 @@ public class Circle extends Entity {
         return triangles;
     }
 
-    private float[] defineBasicColors(int numPoints, RgbaColor fillColor) {
+    /**
+     * Defines the fill color of the circle. The colors are defines for each vertex.
+     * @param numPoints the number of points of the circle
+     * @param fillColor the fill color of the circle
+     * @return a list of rgba colors
+     */
+    private float[] defineFillColors(int numPoints, RgbaColor fillColor) {
         float[] colors = new float[numPoints * 4 + 4];
 
         for (int i = 0; i < numPoints * 4; i+=4) {
